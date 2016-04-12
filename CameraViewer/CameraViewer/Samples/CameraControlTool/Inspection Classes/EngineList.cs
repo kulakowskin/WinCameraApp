4767fb011febb4c954e19b329c41ea0d06f2b0d2
micbentz <micbentz@gmail.com>
2016-04-11 23:01:24 -0400

    Fixes bug where camera view wasnt showing

---
+++ CameraViewer/CameraViewer/Samples/CameraControlTool/Inspection Classes/EngineList.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CameraControlTool.Inspection_Classes
+{
+    class EngineList
+    {
+        private static EngineList instance;
+        private List<String> engineList;
+
+        private EngineList()
+        {
+            this.engineList = new List<String>();
+        }
+
+        public static EngineList getInstance()
+        {
+            if (instance == null)
+            {
+                instance = new EngineList();
+            }
+            return instance;
+        }
+
+        public void addNewEngine(String description)
+        {
+            engineList.Add(description);
+        }
+
+        public void removeEngine(int index)
+        {
+            engineList.RemoveAt(index);
+        }
+    }
+}
